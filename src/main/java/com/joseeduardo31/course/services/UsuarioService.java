69e4c21314bc16a7861567e3fc91b6d39a32fb53
joseeduardo0131gmailcom <joseeduardo0131@gmail.com>
2020-11-16 11:47:16 -0300

    tratamento de exceção - delete

--- src/main/java/com/joseeduardo31/course/services/UsuarioService.java
+++ src/main/java/com/joseeduardo31/course/services/UsuarioService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.joseeduardo31.course.entities.Usuario;
 import com.joseeduardo31.course.repositories.UsuarioRepository;
+import com.joseeduardo31.course.services.exceptions.DatabaseException;
 import com.joseeduardo31.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -29,7 +32,13 @@ public class UsuarioService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public Usuario atualizar(Long id, Usuario obj) {
